fdb3b59cee5c4530abb583a5054a0d9438e0e315
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-10 15:24:05 -0700

    added a test i do not really understand

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,16 +1,32 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/services/githubAuth');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
-  });
+
   afterAll(() => {
     pool.end();
+  });
+
+
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
